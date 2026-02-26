fa6b4071f8c08175029018984dc6702818f1b011
RizkyFirdiansyah <mr.frdsyh@gmail.com>
2026-02-26 23:10:57 +0700

    P2_Views

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>POS - Home</title>
+</head>
+<body>
+  <h1>Welcome to POS System</h1>
+  <p>This is the home page of the Point of Sale system.</p>
+</body>
+</html>
