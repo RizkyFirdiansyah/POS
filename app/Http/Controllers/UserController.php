14279af229cd58eb7b3663e02551f4fe82c60872
RizkyFirdiansyah <mr.frdsyh@gmail.com>
2026-03-09 11:59:09 +0700

    P4_Relationships

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,10 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::all();
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::with('level')->get();
         return view('user', ['data' => $user]);
     }
 
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -15,6 +15,8 @@
       <td>Username</td>
       <td>Nama</td>
       <td>ID Level Pengguna</td>
+      <td>Kode Level</td>
+      <td>Nama Level</td>
       <td>Aksi</td>
     </tr>
     @foreach ($data as $data)
@@ -23,6 +25,8 @@
       <td>{{ $data->username }}</td>
       <td>{{ $data->nama }}</td>
       <td>{{ $data->level_id }}</td>
+      <td>{{ $data->level->level_kode }}</td>
+      <td>{{ $data->level->level_nama }}</td>
       <td>
         <a href="user/ubah/{{ $data->user_id }}">Ubah</a> |
         <a href="user/hapus/{{ $data->user_id }}">Hapus</a>
